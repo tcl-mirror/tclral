7581e80b2cef115d1eafa61beb39d6e5add4ca04
andrewm <andrewm>
2014-12-22 01:10:18 +0000

    Commiting build for MacOSX.

--- build/macosx10.10-i386-x86_64/pkgIndex.tcl
+++ build/macosx10.10-i386-x86_64/pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded ral 0.11.3 \
-    [list load [file join $dir libral0.11.3.dylib]]\n[list source [file join $dir ral.tcl]]
+package ifneeded ral 0.11.4 \
+    [list load [file join $dir libral0.11.4.dylib]]\n[list source [file join $dir ral.tcl]]
