ef336c8b6012dd5baacd0893f899019f8c84696c
andrewm <andrewm>
2015-02-02 01:14:48 +0000

    Committing build of TclRAL 0.11.5 for linux and windows.

--- build/ix86-linux-tcl8.6/pkgIndex.tcl
+++ build/ix86-linux-tcl8.6/pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded ral 0.11.4 \
-    [list load [file join $dir libral0.11.4.so]]\n[list source [file join $dir ral.tcl]]
+package ifneeded ral 0.11.5 \
+    [list load [file join $dir libral0.11.5.so]]\n[list source [file join $dir ral.tcl]]
--- build/x86_64-win64-tcl8.6/pkgIndex.tcl
+++ build/x86_64-win64-tcl8.6/pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded ral 0.11.4 \
-    [list load [file join $dir ral0114.dll]]\n[list source [file join $dir ral.tcl]]
+package ifneeded ral 0.11.5 \
+    [list load [file join $dir ral0115.dll]]\n[list source [file join $dir ral.tcl]]
