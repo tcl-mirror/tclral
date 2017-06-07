bf41af9186779ceb6ebc49b8cb5d2f9ca5468bd3
andrewm <andrewm>
2017-06-07 18:23:27 +0000

    Build of version 0.12.1 for macos.

--- build/macosx10.10-i386-x86_64/pkgIndex.tcl
+++ build/macosx10.10-i386-x86_64/pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded ral 0.12.0 \
-    [list load [file join $dir libral0.12.0.dylib]]\n[list source [file join $dir ral.tcl]]
+package ifneeded ral 0.12.1 \
+    [list load [file join $dir libral0.12.1.dylib]]\n[list source [file join $dir ral.tcl]]
